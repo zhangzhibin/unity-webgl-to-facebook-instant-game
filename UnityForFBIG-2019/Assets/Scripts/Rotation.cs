020cab192818ce54bfa85481cb4b296a710391e2
abin@minigame <zhangzhibin@gmail.com>
2021-06-24 15:52:21 +0800

    add Unity 2019.4 Template Project

---
+++ UnityForFBIG-2019/Assets/Scripts/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dt = Time.deltaTime;
+
+        gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 60 * dt);
+    }
+}
